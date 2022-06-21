7484c1b699e0fb78d1f67a2d24940a0bd978d1ce
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-21 18:07:24 +0300

    Initial Commit

---
+++ Names to Faces prj10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Names to Faces prj10
+//
+//  Created by Nikita  on 6/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
